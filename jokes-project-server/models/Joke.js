0c29652e398e0c04a7aee246eaab57ff80144519
debirupalit <debirupa030101@gmail.com>
2025-03-17 23:06:29 +0530

    commit

---
+++ jokes-project-server/models/Joke.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const jokeSchema = new mongoose.Schema({
+  setup: { 
+    type: String, 
+    required: true 
+  },
+  punchline: { 
+    type: String, 
+    required: true 
+  },
+  type: { 
+    type: String, 
+    required: true 
+  },
+});
+
+module.exports = mongoose.model("Joke", jokeSchema);
