6b7423f6b531cf606362841298c2accb0423b777
debirupalit <debirupa030101@gmail.com>
2025-03-18 21:38:10 +0530

    some changes

--- jokes-project-server/routers/jokeRouter.js
+++ jokes-project-server/routers/jokeRouter.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const { getJokes, addJoke, deleteJoke } = require("../controllers/jokeController");
+const { getJokes } = require("../controllers/jokeController");
 
 const router = express.Router();
 
 // Define routes
 router.get("/", getJokes);
-router.post("/", addJoke);
-router.delete("/:id", deleteJoke);
 
 module.exports = router;
